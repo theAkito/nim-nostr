19b263488e57bce4bb01ef9bae2f9623edc6832b
Akito <the@akito.ooo>
2023-11-01 16:05:10 +0100

    Add Constructor Event

--- nostr/protocol/model/event.nim
+++ nostr/protocol/model/event.nim
@@ -1,14 +1,14 @@
-import ../../[ utils, sha], std/[ json, with ]
+import ../../[ utils, sha ], std/[ strutils, json, with, times ]
 
 type
   Event* = object       ## https://github.com/nostr-protocol/nips/blob/master/01.md#events-and-signatures
     id*: string         ## 32-bytes lowercase hex-encoded sha256 of the serialized event data
     pubkey*: string     ## 32-bytes lowercase hex-encoded public key of the event creator
-    created_at*: string ## UNIX Timestamp in seconds
+    created_at*: int64  ## UNIX Timestamp in seconds
     kind*: int          ## Integer between 0 and 65535
     tags*: seq[string]  ## Arbitrary String
     content*: string    ## Arbitrary String
-    sig*: string        ## 64-bytes lowercase hex of the signature of the sha256 hash of the serialized event data, which is the same as the "id" field
+    sig*: string        ## 64-bytes lowercase hex of the signature applied to the sha256 hash of the `id` field
 
 func obtainEventIdJArray(event: Event): JsonNode =
   result = newJArray()
@@ -30,3 +30,20 @@ func obtainEventId(event: Event): string =
 func setEventId(event: Event): Event =
   event.edit:
     it.id = event.obtainEventId
+
+proc constructEvent*(
+  pubkey: string,
+  kind: int,
+  tags: seq[string],
+  content: string,
+  sig: string
+): Event =
+  Event(
+    id: string.default,
+    pubkey: pubkey.toHex,
+    created_at: getTime().toUnix,
+    kind: kind,
+    tags: tags,
+    content: content,
+    sig: sig
+  ).setEventId
