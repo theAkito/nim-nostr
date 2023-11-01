fa6b60589b264937cbd372951117106d743a2100
Akito <the@akito.ooo>
2023-11-01 16:12:14 +0100

    Update Nostr Event

--- nostr/protocol/model/event.nim
+++ nostr/protocol/model/event.nim
@@ -1,14 +1,14 @@
 import ../../[ utils, sha ], std/[ strutils, json, with, times ]
 
 type
-  Event* = object       ## https://github.com/nostr-protocol/nips/blob/master/01.md#events-and-signatures
-    id*: string         ## 32-bytes lowercase hex-encoded sha256 of the serialized event data
-    pubkey*: string     ## 32-bytes lowercase hex-encoded public key of the event creator
-    created_at*: int64  ## UNIX Timestamp in seconds
-    kind*: int          ## Integer between 0 and 65535
-    tags*: seq[string]  ## Arbitrary String
-    content*: string    ## Arbitrary String
-    sig*: string        ## 64-bytes lowercase hex of the signature applied to the sha256 hash of the `id` field
+  Event* = object           ## https://github.com/nostr-protocol/nips/blob/master/01.md#events-and-signatures
+    id*: string             ## 32-bytes lowercase hex-encoded sha256 of the serialized event data
+    pubkey*: string         ## 32-bytes lowercase hex-encoded public key of the event creator
+    created_at*: int64      ## UNIX Timestamp in seconds
+    kind*: int              ## Integer between 0 and 65535
+    tags*: seq[seq[string]] ## Array of String Arrays of arbitrary size
+    content*: string        ## Arbitrary String
+    sig*: string            ## 64-bytes lowercase hex of the signature applied to the sha256 hash of the `id` field
 
 func obtainEventIdJArray(event: Event): JsonNode =
   result = newJArray()
@@ -34,7 +34,7 @@ func setEventId(event: Event): Event =
 proc constructEvent*(
   pubkey: string,
   kind: int,
-  tags: seq[string],
+  tags: seq[seq[string]],
   content: string,
   sig: string
 ): Event =
